e0a5af35a54544f0d9af7ffd056c8406c84b00dc
dooyeoung <dooyeoung.ryu@gmail.com>
2020-01-19 16:35:16 +0900

    todolist 초기완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
+import TodoList from './components/TodoList'
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header"> 
+      hellowyarn 
       </header>
+      <TodoList/>
     </div>
   );
 }
